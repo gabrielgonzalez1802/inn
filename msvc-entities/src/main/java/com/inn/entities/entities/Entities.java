3bb1dde1e68d4bfbaa8eb769ba2cfaf0020854f3
Gabriel Gonzalez <gabrielgonzalez1802@gmail.com>
2025-02-23 20:19:57 -0400

    refactor: se modifica  entities

--- msvc-entities/src/main/java/com/inn/entities/entities/Entities.java
+++ msvc-entities/src/main/java/com/inn/entities/entities/Entities.java
@@ -1,9 +1,15 @@
 package com.inn.entities.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -31,7 +37,10 @@ public class Entities {
     @Column(name = "entity_phone", nullable = false, length = 15)
     private String entityPhone;
     
+    @Column(name = "entity_type_id", nullable = false)
+    private Long entityTypeId; 
+    
     @ManyToOne
-    @JoinColumn(name = "entity_type_id", nullable = false)
+	@JoinColumn(name = "entity_type_id", referencedColumnName = "entity_type_id", nullable = false, insertable = false, updatable = false)
     private EntitiesType entityType;
 }

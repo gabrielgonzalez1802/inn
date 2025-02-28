ccd2e75dcfd096165c9ce01a17412fbaba01ef9b
gioan robles <gioan2908@gmail.com>
2025-02-27 22:08:59 -0400

    fix: base de datos separadas dockerizadas. estructura original en los properties

---
+++ init.sql
@@ -0,0 +1,9 @@
+
+  CREATE DATABASE system_users;
+  CREATE DATABASE payments;
+  CREATE DATABASE products;
+  CREATE DATABASE orders;
+  CREATE DATABASE entities;
+  CREATE DATABASE address;
+  CREATE DATABASE warehouses;
+
